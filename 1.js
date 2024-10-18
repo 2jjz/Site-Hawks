1a9de65ff2e5b32fdabc7ba53f79bb55197e5f22
Leonardo Yukio <135387822+2jjz@users.noreply.github.com>
2024-10-18 07:27:46 -0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,33 @@
+window.local_easyAccordion_1_data = [
+{
+ heading: "MECANICA GERAL",
+ link: "http://www.xara.com",
+ link_title: " ",
+ text: "Excel\u00EAncia e Tecnologia a sua disposi\u00E7\u00E3o\u000A\u000AContamos com os mais avan\u00E7ados equipamentos para todos os tipos de servi\u00E7os, buscando oferecer o que h\u00E1 de melhor aos nossos clientes.",
+ title: "Mec\u00E2nica Geral"
+},
+{
+ heading: "SUSPENS\u00C3O",
+ text: "Main Text.  Edit this text as required.  Amet cillum labore proident sunt consequat laboris nulla amet dolore, sit ut do veniam, sed cillum. Magna consectetur adipisicing ad dolore in in cillum proident. Veniam ea reprehenderit.",
+ title: "Suspens\u00E3o"
+},
+{
+ heading: "TROCA DE EMBREAGEM",
+ text: "Main Text.  Edit this text as required.  Amet cillum labore proident sunt consequat laboris nulla amet dolore, sit ut do veniam, sed cillum. Magna consectetur adipisicing ad dolore in in cillum proident. Veniam ea reprehenderit.",
+ title: "Troca de Embreagem"
+},
+{
+ heading: "SISTEMA DE ARREFECIMENTO",
+ link: "http://www.xara.com",
+ link_title: " ",
+ text: "Main Text.  Edit this text as required.  Amet cillum labore proident sunt consequat laboris nulla amet dolore, sit ut do veniam, sed cillum. Magna consectetur adipisicing ad dolore in in cillum proident. Veniam ea reprehenderit.",
+ title: "Sistema de Arrefecimento"
+},
+{
+ heading: "INJE\u00C7\u00C3O ELETR\u00D4NICA",
+ link: "http://www.xara.com",
+ link_title: "\u000A",
+ text: "Main Text.  Edit this text as required.  Amet cillum labore proident sunt consequat laboris nulla amet dolore, sit ut do veniam, sed cillum. Magna consectetur adipisicing ad dolore in in cillum proident. Veniam ea reprehenderit.",
+ title: "Inje\u00E7\u00E3o Eletr\u00F4nica"
+}
+];
---
+++ 2.js
@@ -0,0 +1,36 @@
+window.local_slidorionleft_0_data = [
+{
+ autoscroll: "10",
+ effect: "1"
+},
+{
+ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ image: "index_htm_files/1249.jpg",
+ title: "Mec\u00E2nica Geral"
+},
+{
+ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ image: "index_htm_files/1250.jpg",
+ title: "Suspens\u00E3o"
+},
+{
+ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ image: "index_htm_files/1251.jpg",
+ title: "Troca de Embreagem"
+},
+{
+ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ image: "index_htm_files/1252.jpg",
+ title: "Sistema de Arrefecimento"
+},
+{
+ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ image: "index_htm_files/1253.jpg",
+ title: "Inje\u00E7\u00E3o Eletr\u00F4nica"
+},
+{
+ content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+ image: "index_htm_files/1254.jpg",
+ title: "Diagn\u00F3stico"
+}
+];
